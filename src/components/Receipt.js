762a04bd22aaa41174db5b252c26ac03d32c4fa3
shirley54reece <shirley54reece@outlook.com>
2026-08-12 14:04:13 +0530

    Create Receipt.js

---
+++ src/components/Receipt.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Receipt.css';
+
+function Receipt() {
+  return (
+    <div className="receipt">
+      <h2>Receipts</h2>
+      <p>No receipts uploaded yet.</p>
+    </div>
+  );
+}
+
+export default Receipt;
